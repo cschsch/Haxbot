3969a3bc068e6ada68efb3dd68bb89955391ece6
csch <cschwarzbeck@TECHNIK-CON>
2022-02-09 11:06:30 +0100

    Switching from Alias to auth

--- Haxbot/Entities/Player.cs
+++ Haxbot/Entities/Player.cs
@@ -6,15 +6,16 @@ public class Player : Entity
 {
     [Required]
     public string Name { get; set; }
+    [Required]
+    public string Auth { get; set; }
     public virtual ICollection<Team> Teams { get; set; }
-    public virtual ICollection<Alias> Aliases { get; set; }
 
-    public Player(string name) : base()
+    public Player(string name, string auth) : base()
     {
         Name = name;
+        Auth = auth;
         Teams = new HashSet<Team>();
-        Aliases = new HashSet<Alias>();
     }
 
-    public Player() : this("") { }
+    public Player() : this("", "") { }
 }
