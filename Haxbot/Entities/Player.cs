300d63f78c9bd19978ad560bb27da40b027ad6f4
csch <cschwarzbeck@TECHNIK-CON>
2022-02-15 13:03:03 +0100

    Adding index on Auth

--- Haxbot/Entities/Player.cs
+++ Haxbot/Entities/Player.cs
@@ -1,7 +1,9 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
 
 namespace Haxbot.Entities;
 
+[Index(nameof(Auth))]
 public class Player : Entity
 {
     [Required]
