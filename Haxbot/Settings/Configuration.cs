80b5f8cbc7e4d500f434055c640b4c6c923b4b17
csch <cschwarzbeck@TECHNIK-CON>
2022-04-04 09:34:04 +0200

    Adding Chromium args to configuration

--- Haxbot/Settings/Configuration.cs
+++ Haxbot/Settings/Configuration.cs
@@ -7,6 +7,7 @@ public record Configuration
     public string DatabasePath { get; init; }
     public string ConnectionStringTemplate { get; init; }
     public string HaxballHeadlessUrl { get; init; }
+    public string[] ChromiumArgs { get; init; }
     public RoomConfiguration RoomConfiguration { get; init; }
     public string[] RoomAdmins { get; init; }
     public string BotOwner { get; init; }
@@ -19,6 +20,7 @@ public record Configuration
         DatabasePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), nameof(Haxbot), "haxbot.db");
         ConnectionStringTemplate = "Data Source={0}";
         HaxballHeadlessUrl = "https://html5.haxball.com/headless";
+        ChromiumArgs = new[] { "--disable-features=WebRtcHideLocalIpsWithMdns", "--no-sandbox", "--no-proxy-server" };
         RoomConfiguration = new RoomConfiguration();
         RoomAdmins = Array.Empty<string>();
         BotOwner = string.Empty;
