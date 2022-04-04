bf12ff14fd9cc04a60f0efa4f08bae2ca0d1d0f0
csch <cschwarzbeck@TECHNIK-CON>
2022-04-04 14:28:55 +0200

    Simplifying CommandNode

--- CLI/CommandNode.cs
+++ CLI/CommandNode.cs
@@ -3,29 +3,14 @@ using System.CommandLine;
 
 namespace CLI;
 
-public class CommandNode : ICollection<CommandNode>
+public class CommandNode : IEnumerable<CommandNode>
 {
     public Command Command { get; init; }
     private List<CommandNode> SubCommands { get; } = new List<CommandNode>();
 
     public CommandNode(Command command) => Command = command;
 
-    public int Count => 1 + SubCommands.Sum(commandNode => commandNode.Count);
-    public bool IsReadOnly => false;
-
     public void Add(CommandNode item) => SubCommands.Add(item);
-
-    public void Clear() => SubCommands.Clear();
-
-    public bool Contains(CommandNode item) => SubCommands.Contains(item);
-
-    public void CopyTo(CommandNode[] array, int arrayIndex) => SubCommands.CopyTo(array, arrayIndex);
-
-    public IEnumerator<CommandNode> GetEnumerator() => SubCommands.GetEnumerator();
-
-    public bool Remove(CommandNode item) => SubCommands.Remove(item);
-
-    IEnumerator IEnumerable.GetEnumerator() => SubCommands.GetEnumerator();
 
     public static implicit operator CommandNode(Command command) => new(command);
 
@@ -37,4 +22,7 @@ public class CommandNode : ICollection<CommandNode>
             command.InitializeCommand();
         }
     }
+
+    public IEnumerator<CommandNode> GetEnumerator() => SubCommands.GetEnumerator();
+    IEnumerator IEnumerable.GetEnumerator() => SubCommands.GetEnumerator();
 }
