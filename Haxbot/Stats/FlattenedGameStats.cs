e11da4649490b16180d1d96687e39630cd714fec
csch <cschwarzbeck@TECHNIK-CON>
2022-04-13 12:24:17 +0200

    Adding Add for FlattenedGameStats

--- Haxbot/Stats/FlattenedGameStats.cs
+++ Haxbot/Stats/FlattenedGameStats.cs
@@ -5,7 +5,18 @@ public record FlattenedGameStats : GameStats
     public DateTime? Date { get; set; }
     public Stadium? Stadium { get; set; }
 
+    public FlattenedGameStats() { }
+
     public FlattenedGameStats(GameStats stats) : base(stats)
     {
     }
+
+    public FlattenedGameStats Add(FlattenedGameStats stats) => this with
+    {
+        AmountWon = AmountWon + stats.AmountWon,
+        AmountLost = AmountLost + stats.AmountLost,
+        Date = stats.Date,
+        Stadium = stats.Stadium,
+        Identification = stats.Identification
+    };
 }
