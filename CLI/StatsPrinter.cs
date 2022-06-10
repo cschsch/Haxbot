e9d0f1cbba7af7b4d03eda6306ca325f621d6917
csch <cschwarzbeck@TECHNIK-CON>
2022-06-10 15:56:52 +0200

    Moving GetCollector to GroupingHelper

--- CLI/StatsPrinter.cs
+++ CLI/StatsPrinter.cs
@@ -9,27 +9,9 @@ public class StatsPrinter
 {
     public Grouping[] Groupings { get; init; } = Array.Empty<Grouping>();
 
-    private IStatsCollector GetCollector()
-    {
-        if (!GroupingHelper.ResultGroupings.Contains(Groupings.Last())) throw new ArgumentException("Last Grouping needs to be of a result collector");
-        if (Groupings.Count(GroupingHelper.ResultGroupings.Contains) > 1) throw new ArgumentException("There can only be one result collector");
-
-        var collectorTypes = typeof(IStatsCollector)
-            .Assembly
-            .GetTypes()
-            .Where(type => type.Namespace == "Haxbot.Stats" && !type.IsAbstract && !type.IsInterface && type.Name.Contains("StatsCollector"))
-            .ToArray();
-        var completeCollectorType = Groupings
-            .Distinct()
-            .Select(grouping => collectorTypes.Single(type => type.Name.StartsWith(grouping.ToString())))
-            .Reverse()
-            .Aggregate((acc, cur) => cur.MakeGenericType(acc));
-        return (IStatsCollector) Activator.CreateInstance(completeCollectorType)!;
-    }
-
     public void PrintStats(ICollection<Game> games, ICollection<Player> players)
     {
-        var collector = GetCollector();
+        var collector = GroupingHelper.GetCollector(Groupings);
         Parallel.ForEach(games, game => collector.Register(game, players));
         var result = collector.FormatTable(Formatter.Format);
         Console.WriteLine(result);
--- Haxbot/Stats/GroupingHelper.cs
+++ Haxbot/Stats/GroupingHelper.cs
@@ -3,4 +3,22 @@
 public static class GroupingHelper
 {
     public static Grouping[] ResultGroupings { get; } = new Grouping[] { Grouping.Player, Grouping.Team };
+
+    public static IStatsCollector GetCollector(Grouping[] groupings)
+    {
+        if (!ResultGroupings.Contains(groupings.Last())) throw new ArgumentException("Last Grouping needs to be of a result collector");
+        if (groupings.Count(ResultGroupings.Contains) > 1) throw new ArgumentException("There can only be one result collector");
+
+        var collectorTypes = typeof(IStatsCollector)
+            .Assembly
+            .GetTypes()
+            .Where(type => type.Namespace == "Haxbot.Stats" && !type.IsAbstract && !type.IsInterface && type.Name.Contains("StatsCollector"))
+            .ToArray();
+        var completeCollectorType = groupings
+            .Distinct()
+            .Select(grouping => collectorTypes.Single(type => type.Name.StartsWith(grouping.ToString())))
+            .Reverse()
+            .Aggregate((acc, cur) => cur.MakeGenericType(acc));
+        return (IStatsCollector)Activator.CreateInstance(completeCollectorType)!;
+    }
 }
